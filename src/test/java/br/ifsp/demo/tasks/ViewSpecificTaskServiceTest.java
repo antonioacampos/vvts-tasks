aa4658e87a24da4033c9da9be1abae81bad4c36a
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-05-03 18:35:49 -0300

    refactor(task-test): using userId at view specific task test

--- src/test/java/br/ifsp/demo/tasks/ViewSpecificTaskServiceTest.java
+++ src/test/java/br/ifsp/demo/tasks/ViewSpecificTaskServiceTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import jdk.jfr.Description;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ViewSpecificTaskServiceTest {
+    UUID userId1 = UUID.randomUUID();
 
     @Test
     @Tag("@TDD")
@@ -18,10 +20,10 @@ public class ViewSpecificTaskServiceTest {
         TaskService taskService = new TaskService();
         LocalDateTime deadline = LocalDateTime.now().plusDays(1);
 
-        taskService.createTask("Estudar", "Fazer revisão de VVTS", deadline);
-        taskService.createTask("Ler", "Capítulo 4", deadline.plusDays(1));
+        taskService.createTask("Estudar", "Fazer revisão de VVTS", deadline, userId1);
+        taskService.createTask("Ler", "Capítulo 4", deadline.plusDays(1), userId1);
 
-        Task task = taskService.getTask(1);
+        Task task = taskService.getTask(1, userId1);
 
         assertThat(task.getTitle()).isEqualTo("Ler");
         assertThat(task.getDescription()).isEqualTo("Capítulo 4");
