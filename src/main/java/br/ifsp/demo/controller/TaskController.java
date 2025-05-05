3a16a85e4aa0dc015162330da4ba97dfc1028c69
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-04 22:42:40 -0300

    feat: final integration of TaskController with TaskServiceDB using real database

--- src/main/java/br/ifsp/demo/controller/TaskController.java
+++ src/main/java/br/ifsp/demo/controller/TaskController.java
@@ -30,28 +30,29 @@ public class TaskController {
     public ResponseEntity<?> create(@RequestBody CreateTaskDTO task) {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
-        TaskEntity newTask = taskService.create(task.title(), task.description(), task.deadline(), task.estimatedTime(), userId);
+        TaskEntity newTask = taskService.create(
+                task.title(), task.description(), task.deadline(), task.estimatedTime(), userId);
 
-        ResponseTaskDTO response = new ResponseTaskDTO(newTask);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseTaskDTO(newTask), HttpStatus.CREATED);
     }
 
     @PutMapping("/edit/{id}")
     public ResponseEntity<?> edit(@PathVariable UUID id, @RequestBody CreateTaskDTO task) {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
-        TaskEntity edited = taskService.editTask(id, task.title(), task.description(), task.deadline(), userId);
+        TaskEntity edited = taskService.editTask(
+                id, task.title(), task.description(), task.deadline(), userId);
 
-        ResponseTaskDTO response = new ResponseTaskDTO(edited);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseTaskDTO(edited), HttpStatus.OK);
     }
 
     @GetMapping("/get-all")
     public ResponseEntity<?> getAll() {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
-        List<TaskEntity> tasks = taskService.getAllByUser(userId);
-        List<ResponseTaskDTO> response = tasks.stream().map(ResponseTaskDTO::new).collect(Collectors.toList());
+        List<ResponseTaskDTO> response = taskService.getAllByUser(userId).stream()
+                .map(ResponseTaskDTO::new)
+                .collect(Collectors.toList());
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -61,9 +62,7 @@ public class TaskController {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
         TaskEntity task = taskService.getTask(id, userId);
-        ResponseTaskDTO response = new ResponseTaskDTO(task);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseTaskDTO(task), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
@@ -78,7 +77,7 @@ public class TaskController {
     public ResponseEntity<?> markCompleted(@PathVariable UUID id) {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
-        taskService.clockOut(id, userId); // marcando como completed = clockOut
+        taskService.markAsCompleted(id, userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -103,8 +102,38 @@ public class TaskController {
         final UUID userId = authenticationInfoService.getAuthenticatedUserId();
 
         TaskEntity task = taskService.getTask(id, userId);
-        Map<String, Long> response = Map.of("status", task.getTimeSpent());
+        return new ResponseEntity<>(Map.of("status", task.getTimeSpent()), HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    @GetMapping("/check-time-exceeded/{id}")
+    public ResponseEntity<?> checkTimeExceeded(@PathVariable UUID id) {
+        final UUID userId = authenticationInfoService.getAuthenticatedUserId();
+
+        boolean exceeded = taskService.checkForTimeExceeded(id, userId);
+        return new ResponseEntity<>(Map.of("status", exceeded), HttpStatus.OK);
+    }
+
+    @GetMapping("/notify-time-exceeded/{id}")
+    public ResponseEntity<?> notifyTimeExceeded(@PathVariable UUID id) {
+        final UUID userId = authenticationInfoService.getAuthenticatedUserId();
+
+        String notify = taskService.checkAndNotifyTimeExceeded(id, userId);
+        return new ResponseEntity<>(Map.of("status", notify), HttpStatus.OK);
+    }
+
+    @GetMapping("/clock-out-forgotten/{id}")
+    public ResponseEntity<?> clockOutForgotten(@PathVariable UUID id) {
+        final UUID userId = authenticationInfoService.getAuthenticatedUserId();
+
+        String check = taskService.checkForClockOutForgotten(id, userId);
+        return new ResponseEntity<>(Map.of("status", check), HttpStatus.OK);
+    }
+
+    @GetMapping("/clock-out-forgotten-completed-task/{id}")
+    public ResponseEntity<?> clockOutForgottenCompletedTask(@PathVariable UUID id) {
+        final UUID userId = authenticationInfoService.getAuthenticatedUserId();
+
+        String check = taskService.checkForClockOutForgottenInCompletedTask(id, userId);
+        return new ResponseEntity<>(Map.of("status", check), HttpStatus.OK);
     }
 }
