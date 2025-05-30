b522184c9b29b7328eb3c4e7667ba976394c8fc0
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-30 01:59:30 -0300

    test: remove Spring dependency from TaskMutationTest to ensure Pitest execution

--- src/test/java/br/ifsp/demo/tasks/TaskMutationTest.java
+++ src/test/java/br/ifsp/demo/tasks/TaskMutationTest.java
@@ -3,16 +3,12 @@ package br.ifsp.demo.tasks;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class TaskMutationTest {
 
     @Autowired
