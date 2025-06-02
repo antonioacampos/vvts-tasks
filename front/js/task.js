8fa664c03fd9904bf4315f07d6ac9839e0c16356
akira-erick <kajitani.motta@gmail.com>
2025-06-02 20:43:54 -0300

    feat(task-front-end): getting task from click on task on other page

--- front/js/task.js
+++ front/js/task.js
@@ -1,7 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
+    const task = await getTask();
 
-    const taskContainer = document.getElementById('task-container');
+    
+});
+
+async function getTask() {
+    const token = localStorage.getItem('tokenTaskVVTS');
+    if (!token) {
+        window.location.href = './index.html'; 
+        return;
+    }
     const id = sessionStorage.getItem('idTask');
 
-    console.log(id);
-});
+    if (!id) {
+        window.location.href = './tasklist.html'; 
+        return;
+    }
+    
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/task/get/${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            }
+        });
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                window.location.href = './index.html'; 
+                return;
+            } else {
+                console.error('Error fetching task:', response.statusText);
+                alert('Error fetching task. Please try again later.');
+                return;
+            }
+        }
+
+        const taskData = await response.json();
+
+        return taskData;
+
+    } catch (error) {
+        console.error('Error fetching task:', error);
+        alert('Error fetching task. Please try again later.');
+        return;
+    }
+
+}
